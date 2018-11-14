55d71ed22b2b44bcfd002ad4bf1a661e5aeacd1e
PradeepU <PradeepU@KNACKHYDLO127.KNACKHYD.DCHYD.COM>
2018-11-14 17:34:12 +0530

    SomeChanges

--- SpringIntegration-XMLfileUpload/src/main/java/com/knack/integrationClasses/XmlServiceActivator.java
+++ SpringIntegration-XMLfileUpload/src/main/java/com/knack/integrationClasses/XmlServiceActivator.java
@@ -1,9 +1,13 @@
 package com.knack.integrationClasses;
 
+
+
 import org.apache.log4j.Logger;
 import org.apache.log4j.xml.DOMConfigurator;
 
-public class XmlServiceActivator {
+
+public class XmlServiceActivator
+{
 
 	final static Logger logger = Logger.getLogger(XmlServiceActivator.class);
 
